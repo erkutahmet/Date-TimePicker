32db1ad4945916210383b7a1212801fa4c67386c
erkutahmet <90329328+erkutahmet@users.noreply.github.com>
2023-10-17 00:07:29 +0300

    Date&TimePicker

--- Date&TimePicker/Date&TimePicker/ViewController.swift
+++ Date&TimePicker/Date&TimePicker/ViewController.swift
@@ -9,11 +9,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var dateTextField: UITextField!
+    @IBOutlet weak var hourTextField: UITextField!
+    
+    var datePicker: UIDatePicker?
+    var hourPicker: UIDatePicker?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        datePicker = UIDatePicker()
+        datePicker?.datePickerMode = .date
+        dateTextField.inputView = datePicker
+        
+        hourPicker = UIDatePicker()
+        hourPicker?.datePickerMode = .time
+        hourTextField.inputView = hourPicker
+        
+        if #available(iOS 13.4, *) {
+            datePicker?.preferredDatePickerStyle = .wheels
+            hourPicker?.preferredDatePickerStyle = .wheels
+        }
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tapGestureMethod))
+        view.addGestureRecognizer(tapGesture)
+        
+        datePicker?.addTarget(self, action: #selector(showDate(datePicker:)), for: .valueChanged)
+        hourPicker?.addTarget(self, action: #selector(showHour(hourPicker:)), for: .valueChanged)
     }
-
-
+    
+    @objc func tapGestureMethod() {
+        print("Touched screen.")
+        view.endEditing(true)
+    }
+    
+    @objc func showDate(datePicker: UIDatePicker) {
+        let format = DateFormatter()
+        format.dateFormat = "MM/dd/yyyy"
+        let gotDate = format.string(from: datePicker.date)
+        dateTextField.text = gotDate
+    }
+    
+    @objc func showHour(hourPicker: UIDatePicker) {
+        let format = DateFormatter()
+        format.dateFormat = "HH:mm"
+        let gotDate = format.string(from: hourPicker.date)
+        hourTextField.text = gotDate
+    }
 }
 
